ae0d9c8d268c8c3f47e08e3bc19446e68a6e3a8b
Krista Strzeszkowski <krista.cathleen.722@gmail.com>
2018-10-25 15:46:50 -0400

    init commit

---
+++ JoshTraining.js
@@ -0,0 +1,400 @@
+'use strict';
+
+/** 
+ * Given an input value, print the value to the screen using console.log().
+ */
+function print(value) {
+  // YOUR CODE BELOW HERE //
+  console.log(value)
+  // YOUR CODE ABOVE HERE //
+}
+/**
+ * Given an input String, return its length.
+ *
+ * TIP: In JavaScript, how can we decipher the length of a String?
+ * work?
+ */
+function length(string) {
+    // YOUR CODE BELOW HERE //
+    return string.length
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to lowercase.
+ */
+function toLowerCase(string) {
+    // YOUR CODE BELOW HERE //
+    return string.toLowerCase()
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to uppercase.
+ */
+function toUpperCase(string) {
+    // YOUR CODE BELOW HERE //
+        return string.toUpperCase()
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to dash-case.
+ *
+ * Examples:
+ *
+ *      toDashCase('Hello World'); // => 'hello-world'
+ *
+ * TIP: How can you look for and replace a space in a String? How can you
+ *      enforce lowercase? Look at the global replace link below, or, try String
+ *      methods split and join?
+ *
+ *      See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Using_global_and_ignore_with_replace()
+ */
+function toDashCase(string) {
+    // YOUR CODE BELOW HERE //
+    let newstring = string.toLowerCase().replace(/\ /g, "-")
+    return newstring
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * begins with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      beginsWith('Max', 'm'); // => true;
+ *      beginsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function beginsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    if (string.toLowerCase().charAt(0) === char.toLowerCase()) {
+        return true
+    } else {
+        return false
+    }
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * ends with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      endsWith('Max', 'X'); // => true;
+ *      endsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function endsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    if (string.toLowerCase().charAt(string.length - 1) === char.toLowerCase()) {
+        return true
+    } else {
+        return false
+    }
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two input Strings, return the Strings concatenated into one.
+ *
+ * TIP: What's the operator to concatenate two Strings?
+ */
+function concat(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    return stringOne +  stringTwo
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given any number of Strings, return all of them joined together.
+ *
+ * Example:
+ *
+ *      join("my", "name", "is", "Ben"); // => "mynameisBen";
+ *
+ * TIP: This Function pulls out all the arguments passed to it and stores them
+ *      in an Array called args.
+ */
+function join(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    let result = ""
+    for (let index in arguments) {
+        result += arguments[index]
+    }
+    return result
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return the longest of the two.
+ *
+ * Example:
+ *
+ *      longest("ben", "maggie");   //-> "maggie"
+ *
+ * TIP: What property of the String do we need to compare?
+ */
+function longest(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    if (stringOne.length > stringTwo.length) {
+        return stringOne
+    } else {
+        return stringTwo
+    }
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return 1 if the first is higher in alphabetical order than
+ * the second, return -1 if the second is higher in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortAscending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    if (stringOne.toLowerCase().charAt(0) > stringTwo.toLowerCase().charAt(0)) {
+        return 1
+    } else if (stringOne.toLowerCase().charAt(0) < stringTwo.toLowerCase().charAt(0)) {
+        return -1
+    } else {
+        return 0
+    }
+    // YOUR CODE ABOVE HERE //
+}
+
+
+/**
+ * Given two Strings, return 1 if the first is lower in alphabetical order than
+ * the second, return -1 if the second is lower in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortDescending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    if (stringOne.toLowerCase().charAt(0) < stringTwo.toLowerCase().charAt(0)) {
+        return 1
+    } else if (stringOne.toLowerCase().charAt(0) > stringTwo.toLowerCase().charAt(0)) {
+        return -1
+    } else {
+        return 0
+    }
+    // YOUR CODE ABOVE HERE //
+}
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: LOOPS
+ */
+/** 
+ * Given an input Array, loop forward over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValues(array) {
+  // YOUR CODE BELOW HERE // 
+  for (let i = 0; i < array.length; i++) {
+      console.log(array[i])
+  }
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Array, loop backwards over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValuesInReverse(array) {
+  // YOUR CODE BELOW HERE //
+    for (let i = array.length-1; i >= 0; i--) {
+        console.log(array[i])
+    }
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, loop over the Object and print its values 
+ * using console.log().
+ */
+function printObjectValues(object) {
+  // YOUR CODE BELOW HERE //
+    for (let key in object) {
+        console.log(object[key])
+    }
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return an Array containing the Object keys.
+ */
+function getObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+  return Object.keys(object)
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, loop over the Object and print its keys 
+ * using console.log().
+ */
+function printObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+    for(let key in object) {
+        console.log(key)
+    }
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return the length of its key/value pairs
+ */
+function getObjectLength(object) {
+  // YOUR CODE BELOW HERE //
+  return Object.keys(object).length
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, how might we loop over the Object IN REVERSE and 
+ * print its values using console.log()?
+ */
+function printObjectValuesInReverse(object) {
+  // YOUR CODE BELOW HERE //
+  let keys = Object.keys(object)
+  for (let i = keys.length - 1; i >= 0; i--) {
+    console.log(object[keys[i]])
+  }
+  // YOUR CODE ABOVE HERE //
+}
+
+
+// CODE SIGNAL PRACTICE
+
+function centuryFromYear(year) {
+  let lastDigits = year % 100
+  if (lastDigits != 0) {
+    return Math.floor(year / 100 + 1)
+  } else {
+    return Math.floor(year / 100)
+  }
+}
+
+function checkPalindrome(inputString) {
+  if (inputString.length <= 1) {
+    return true
+  }
+  let firstChar = inputString.charAt(0)
+  let lastChar = inputString.charAt(inputString.length - 1)
+  if (firstChar === lastChar) {
+    inputString = inputString.substring(1, inputString.length - 1)
+    return checkPalindrome(inputString)
+  } else {
+    return false    
+  }
+}
+/**
+ * 2: Fizz Buzz
+ *
+ *  a. Write a Function called fizzBuzz that prints the numbers from 1 to 100.
+ *  b. BUT, for multiples of three print “Fizz” instead of the number.
+ *  c. AND for the multiples of five print “Buzz”.
+ *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
+*/
+function fizzBuzz() {
+    // YOUR CODE BELOW HERE //
+    for (let i = 0; i <= 100; i++) {
+         if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz")
+        } else if (i % 3 == 0) {
+            console.log("Fizz")
+        } else if (i % 5 == 0) {
+            console.log("Buzz") 
+        } else {
+            console.log(i)
+        }
+    }
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
+ */
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is greater than the 
+ * base.
+ */
+function createGreaterThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is less than the 
+ * base.
+ */
+function createLessThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a startsWith character, which will be a single character, return a 
+ * Function that tests whether a given String starts with the startsWith 
+ * character.
+ */
+function createStartsWithFilter(startsWith) {
+    // YOUR CODE BELOW HERE //
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a endsWith character, which will be a single character, return a 
+ * Function that tests whether a given String ends with the endsWith 
+ * character.
+ */
+function createEndsWithFilter(endsWith) {
+    // YOUR CODE BELOW HERE //
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to modify a String, 
+ * return an Array of the Strings, modified.
+ * 
+ * TIP: You need to loop over the Strings, right? We need to pass each String to 
+ * the modify Function, but we need to collect the results into some collection.
+ */
+function modifyStrings(strings, modify) {
+    // YOUR CODE BELOW HERE //
+ 
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to test the String in some 
+ * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
+ * 
+ * Imagine you had a list of names, and you wanted to test they all 
+ * begin with "C", or they are all exclaimations that end with "!".
+ * 
+ * TIP: You need to loop over the Strings, right? And pass them to the test?
+ */
+function allStringsPass(strings, test) {
+
+    // YOUR CODE BELOW HERE //
+
+    // YOUR CODE ABOVE HERE //
